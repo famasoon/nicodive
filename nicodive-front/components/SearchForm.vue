d4cf422b4ae7c4c6bc4fdc58f06e30352e5c9baf
famasoon <famasoon@gmail.com>
2019-05-21 21:18:08 +0900

    Index page show Search form

---
+++ nicodive-front/components/SearchForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="field has-addons">
+    <div class="control">
+      <input v-model="name" type="text" class="input" placeholder="Search" />
+    </div>
+    <div class="control">
+      <nuxt-link
+        :to="{ name: 'result-slug', params: { slug: name } }"
+        ,
+        class="button is-info"
+      >
+        GO
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: ""
+    }
+  }
+}
+</script>
