4ed2f6268f3052222890a9b2797fae055a24856d
famasoon <famasoon@gmail.com>
2019-06-02 19:19:33 +0900

    Show title, some counters, and description

--- nicodive-front/pages/video/_videoid.vue
+++ nicodive-front/pages/video/_videoid.vue
@@ -1,5 +1,13 @@
 <template>
-  <section class="container">
+  <section class="container is-fluid">
+    <h1 class="title">
+      {{ resp.video.title }}
+    </h1>
+    <div class="tags is-rounded">
+      <span class="tag">再生数: {{ resp.video.view_counter }}</span>
+      <span class="tag">マイリスト数: {{ resp.video.mylist_counter }}</span>
+    </div>
+    <div class="notification">説明文: {{ resp.video.description }}</div>
     <div id="video">
       {{ resp }}
     </div>
@@ -14,7 +22,6 @@ const VIDEO_API_URL = "http://localhost:8080/v1/video/"
 export default {
   async asyncData(context) {
     const videoID = context.params.videoid
-    console.log(videoID)
     const { data } = await axios.get(VIDEO_API_URL + videoID)
     return { resp: data }
   }
