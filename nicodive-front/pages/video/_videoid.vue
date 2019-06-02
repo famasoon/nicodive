b8ec0ce59ec148108fdaaae9ccc563defa4832d0
famasoon <famasoon@gmail.com>
2019-06-02 21:31:10 +0900

    Fix embed player size

--- nicodive-front/pages/video/_videoid.vue
+++ nicodive-front/pages/video/_videoid.vue
@@ -8,12 +8,15 @@
       <span class="tag">マイリスト数: {{ resp.video.mylist_counter }}</span>
     </div>
     <div class="notification">説明文: {{ resp.video.description }}</div>
-    <iframe
-      class="embed-responsive-item"
-      allowfullscreen
-      frameborder="0"
-      :src="'http://embed.nicovideo.jp/watch/' + $route.params.videoid"
-    />
+    <div class="video-container">
+      <iframe
+        width="640"
+        height="360"
+        allowfullscreen
+        frameborder="0"
+        :src="'http://embed.nicovideo.jp/watch/' + $route.params.videoid"
+      />
+    </div>
     <div id="video">
       {{ resp }}
     </div>
@@ -33,3 +36,22 @@ export default {
   }
 }
 </script>
+
+<style>
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%; /*16:9*/
+  padding-top: 30px;
+  height: 0;
+}
+
+.video-container iframe,
+.video-container object,
+.video-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 50%;
+}
+</style>
