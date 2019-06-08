2b3264510e56d97b6e34d74d76f326fede0851af
famasoon <famasoon@gmail.com>
2019-06-08 19:47:27 +0900

    Add nuxt-link on ranking

--- nicodive-front/pages/index.vue
+++ nicodive-front/pages/index.vue
@@ -1,26 +1,30 @@
 <template>
   <section id="ranking" class="container">
     <div v-for="(item, index) in resp" :key="index" class="card">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left">
-            <h2 class="title">{{ item.rank }}位</h2>
+      <nuxt-link
+        :to="{ name: 'video-videoid', params: { videoid: item.link } }"
+      >
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <h2 class="title">{{ item.rank }}位</h2>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">
+                {{ item.title }}
+              </p>
+            </div>
           </div>
-          <div class="media-content">
-            <p class="title is-4">
-              {{ item.title }}
-            </p>
+          <div id="thumbnail" class="card-image">
+            <figure class="image is-128x128">
+              <img :src="item.thumbnail" alt="Ranking item thumbnail" />
+            </figure>
+          </div>
+          <div class="content has-text-dark">
+            {{ item.desc }}
           </div>
         </div>
-        <div class="card-image" id="thumbnail">
-          <figure class="image is-128x128">
-            <img :src="item.thumbnail" alt="Ranking item thumbnail" />
-          </figure>
-        </div>
-        <div class="content">
-          {{ item.desc }}
-        </div>
-      </div>
+      </nuxt-link>
     </div>
   </section>
 </template>
